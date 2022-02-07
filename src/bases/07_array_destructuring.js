46995b37b6d734d7d61a35aac2a723df16038b91
HarmonySkull <developer@harmonyskull.codes>
2022-02-07 01:50:09 -0500

    Test_Array_Destructuring

--- src/bases/07_array_destructuring.js
+++ src/bases/07_array_destructuring.js
@@ -1,27 +1,10 @@
 const characters = [ 'Goku', 'Vegeta', 'Trunks' ]
 const [ , , p3 ] = characters
-console.log( p3 )
 
-const retornaArreglo = () => {
+export const returnArray = () => {
 	return [ 'ABC', 123 ]
 }
 
-const [ letters, numbers ] = retornaArreglo()
-console.log( letters, numbers )
 
-// Tarea
-// 1. el primer valor del arr se llamará nombre
-// 2. se llamará setNombre
-const useState = ( valor ) => {
-	return [ valor, () => { console.log( 'Hola Mundo' ) } ]
-}
-
-const [ nombre, setNombre ] = useState( 'Goku' )
-
-console.log( nombre )
-setNombre()
-
-
-
-
+const [ letters, numbers ] = returnArray()
 
