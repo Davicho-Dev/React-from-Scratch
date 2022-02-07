72e79171dbdd51513f443c70333a4e9c4a58bb02
HarmonySkull <developer@harmonyskull.codes>
2022-02-06 23:32:07 -0500

    10_Hooks_useState

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,6 +1,13 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
 export const CounterApp = ( { value } ) => {
+	const [ counter, setCounter ] = useState( value )
+
+	const hdlAdd = () => {
+		// setCounter( counter + 1 )
+		setCounter( (count)=> count+ 1 )
+	}
 
 	const hdlAdd = (e) => {
 		console.log(e)
@@ -9,7 +16,7 @@ export const CounterApp = ( { value } ) => {
 	return <>
 		<h1>CounterApp</h1>
 		<hr/>
-		<h2>{ value }</h2>
+		<h2>{ counter }</h2>
 		<button onClick={ hdlAdd }>+1</button>
 	</>
 }
