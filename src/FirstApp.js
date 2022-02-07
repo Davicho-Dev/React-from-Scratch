b2a945b1ea2da7fdb039db083bdc96eeee71ae72
HarmonySkull <developer@harmonyskull.codes>
2022-02-06 20:40:28 -0500

    04_Print_Vars

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,7 +1,19 @@
 // import React, { Fragment } from 'react'
 
-export const FirstApp = () => <>
-	<h1>Hi</h1>
-	<hr/>
-	<p>My first app</p>
-</>
+export const FirstApp = () => {
+	const sayHello = 'Hello World'
+	// const sayHello = 123.23213213
+	// const sayHello = true
+	// const sayHello = [1,2,3,4,5,6]
+	// const sayHello = {
+	// 	name: 'David',
+	// 	age: 30,
+	// }
+
+	return <>
+		<h1>{ sayHello }</h1>
+		{/*<pre>{ JSON.stringify(sayHello, null, 3) }</pre>*/ }
+		<hr/>
+		<p>My first app</p>
+	</>
+}
