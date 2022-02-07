f8e9a1e0d46542986f5e62536831a0101d253a9e
HarmonySkull <developer@harmonyskull.codes>
2022-02-06 22:29:04 -0500

    09_onClick_Event

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types'
 
 export const CounterApp = ( { value } ) => {
+
+	const hdlAdd = (e) => {
+		console.log(e)
+	}
+
 	return <>
 		<h1>CounterApp</h1>
 		<hr/>
 		<h2>{ value }</h2>
+		<button onClick={ hdlAdd }>+1</button>
 	</>
 }
 
