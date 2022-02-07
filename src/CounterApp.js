f14693bf06ae46626e609e7a21ea6e9cd839f1d9
HarmonySkull <developer@harmonyskull.codes>
2022-02-06 23:36:27 -0500

    11_HdlSubtract_HdlReset

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -4,23 +4,26 @@ import { useState } from 'react'
 export const CounterApp = ( { value } ) => {
 	const [ counter, setCounter ] = useState( value )
 
-	const hdlAdd = () => {
-		// setCounter( counter + 1 )
-		setCounter( (count)=> count+ 1 )
-	}
+	const hdlAdd = () => setCounter( counter + 1 )
 
-	const hdlAdd = (e) => {
-		console.log(e)
-	}
+	const hdlSubtract = () => setCounter( counter - 1 )
+
+	const hdlReset = () => setCounter( value )
 
 	return <>
 		<h1>CounterApp</h1>
 		<hr/>
 		<h2>{ counter }</h2>
 		<button onClick={ hdlAdd }>+1</button>
+		<button onClick={ hdlReset }>Reset</button>
+		<button onClick={ hdlSubtract }>-1</button>
 	</>
 }
 
 CounterApp.propTypes = {
 	value: PropTypes.number,
+}
+
+CounterApp.defaultProps = {
+	value: 10
 }
