d38550efad86ace1845e18c29fd2a4ff827dbc6a
HarmonySkull <developer@harmonyskull.codes>
2022-02-06 21:31:41 -0500

    06_PropTypes

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,22 +1,17 @@
-// import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
 
-export const FirstApp = ( { greeting='Hello World!' } ) => {
-	// export const FirstApp = (props) => {
-	// const sayHello = 'Hello World'
-	// const sayHello = 123.23213213
-	// const sayHello = true
-	// const sayHello = [ 1, 2, 3, 4, 5, 6 ]
-	// const sayHello = {
-	// 	name: 'David',
-	// 	age: 30,
-	// }
-	// console.log(props)
+export const FirstApp = ( { greeting } ) => {
+
+	// if ( !greeting ) throw new Error( 'The greeting is necessary!' )
 
 	return <>
 		<h1>{ greeting }</h1>
-		{/*<h1>{ sayHello }</h1>*/ }
-		{/*<pre>{ JSON.stringify(sayHello, null, 3) }</pre>*/ }
 		<hr/>
 		<p>My first app</p>
 	</>
+}
+
+FirstApp.propTypes = {
+	greeting: PropTypes.string.isRequired,
+	// other: PropTypes.number,
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 ReactDOM.render( <StrictMode>
-		<FirstApp/>
-		{/*<FirstApp greeting={ `Hello, i'm Goku` }/>*/}
+		{/*<FirstApp/>*/}
+		<FirstApp greeting={ "Hello, i'm Goku" }/>
+		{/*<FirstApp greeting={ 123 }/>*/}
 	</StrictMode>
 	, document.querySelector( '#root' ) )
