254499b3545fb7b9b09453c87a9948bc6ac1904c
HarmonySkull <developer@harmonyskull.codes>
2022-02-09 17:15:39 -0500

    03_Create_Category_List

--- src/components/pages/GifExpert.js
+++ src/components/pages/GifExpert.js
@@ -1,8 +1,18 @@
 import { useState } from 'react'
 
 export const GifExpert = () => {
+	const hdlAdd = () => {
+	}
+
 	return <>
 		<h2>GifExpert</h2>
 		<hr/>
+		<button  onClick={hdlAdd}>Add</button>
+		<ol>
+			{ categories.map( ( name, index ) =>
+				<li key={ `item-${ index }-${ name }` }>{ name }</li>,
+			)
+			}
+		< /ol>
 	</>
 }
