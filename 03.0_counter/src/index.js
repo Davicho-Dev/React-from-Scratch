2def02dac11ff2d01560f03ef054218bf4f071bb
HarmonySkull <developer@harmonyskull.codes>
2022-06-21 00:44:05 -0500

    CHANGE: 03 Counter App;

--- 03.0_counter/src/index.js
+++
@@ -1,9 +0,0 @@
-import { CounterApp } from './CounterApp'
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-
-ReactDOM.render( <StrictMode>
-		<CounterApp value={ 6543 }/>
-	</StrictMode>
-	, document.querySelector( '#root' ) )
