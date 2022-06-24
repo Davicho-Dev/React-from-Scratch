ff72a63c9d5a230c1f2de65aa8a57f4a8d651821
HarmonySkull <developer@harmonyskull.codes>
2022-06-24 18:07:16 -0500

    CREATE: Template string test;
    Signed-off-by: HarmonySkull <developer@harmonyskull.codes>

--- 03.1_Testing/src/api/config.js
+++
@@ -1,3 +0,0 @@
-const config = { GIPHY_API_KEY: 'api_key=xOGPA4J9seMma4rgsruXb7IO9WAhx5El' }
-
-export default config
--- 03.1_Testing/src/bases/08_import_export.js
+++
@@ -1,6 +0,0 @@
-import { heroesData } from '../data/heroesData'
-
-export const getHeroById = ( id ) => heroesData.find( ( hero ) => hero.id === id )
-
-export const getHeroesByOwner = ( owner ) => heroesData.filter( ( hero ) => hero.owner === owner )
-
