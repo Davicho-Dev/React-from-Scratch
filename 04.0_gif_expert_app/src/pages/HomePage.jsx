b61b6cab6a3497f4812be69e208c12ec3c256579
HarmonySkull <13292121+HarmonySkull@users.noreply.github.com>
2022-08-14 18:51:04 -0500

    ADD: onAddCategory func;
    Signed-off-by: HarmonySkull <13292121+HarmonySkull@users.noreply.github.com>

--- 04.0_gif_expert_app/src/pages/HomePage.jsx
+++ 04.0_gif_expert_app/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import {useState} from 'react'
 
+import {v4} from 'uuid'
+
 const HomePage = () => {
+	const [ categories, setCategories ] = useState( [ 'One Punch' ] )
+
 	return ( <section>
 		<h1>Gif Expert App</h1>
+
+
 	</section> )
 }
 
