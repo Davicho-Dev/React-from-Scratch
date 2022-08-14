aef1b641117ad9ed312890f632c2aa29efba7ad6
HarmonySkull <13292121+HarmonySkull@users.noreply.github.com>
2022-08-14 18:58:51 -0500

    CREATE: GifItem;
    Signed-off-by: HarmonySkull <13292121+HarmonySkull@users.noreply.github.com>

---
+++ 04.0_gif_expert_app/src/components/molecules/GifItem.jsx
@@ -0,0 +1,8 @@
+const GifItem = ( { title, url } ) => {
+	return <figure className='card'>
+		<img src={ url } alt={ title }/>
+		<figcaption>{ title }</figcaption>
+	</figure>
+}
+
+export default GifItem
--- 04.0_gif_expert_app/src/components/molecules/index.js
+++ 04.0_gif_expert_app/src/components/molecules/index.js
@@ -1,1 +1,2 @@
 export {default as AddCategory} from './AddCategory'
+export {default as GifItem} from './GifItem'
