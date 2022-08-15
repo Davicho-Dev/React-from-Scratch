f1bda9a9fb8ae2dc765cdf4cfbb44f3b9074c37b
HarmonySkull <13292121+HarmonySkull@users.noreply.github.com>
2022-08-14 19:24:42 -0500

    ADD: Testing environment config;
    Signed-off-By: HarmonySkull <13292121+HarmonySkull@users.noreply.github.com>

--- 04.1_Testing/src/setupTests.js
+++
@@ -1,6 +0,0 @@
-import { createSerializer } from 'enzyme-to-json'
-import Adapter from 'enzyme-adapter-react-16'
-import { configure } from 'enzyme'
-
-configure( { adapter: new Adapter() } )
-expect.addSnapshotSerializer( createSerializer( { mode: 'deep' } ) )
