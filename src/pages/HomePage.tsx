31d7d58f4f02320f90bc7e408912408a2e5ad268
David Gavilanez <13292121+Davicho-Dev@users.noreply.github.com>
2025-01-13 00:29:36 +0000

    feat(Characters): Added the call of the characters and presented the list on the main page;
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: HarmonySkull <109529692+HarmonySkull@users.noreply.github.com>
    Co-authored-by: Davicho-Dev <13292121+DavichoDev@users.noreply.github.com>
    Signed-off-by: David Gavilanez <13292121+Davicho-Dev@users.noreply.github.com>

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import { v4 } from 'uuid';
+
+import { CharacterCard } from '@molecules';
+import { getCharacters } from '@services';
+
+import type { ICharactersProps } from '@interfaces';
+
 const HomePage = () => {
-	return <h1>Home Page</h1>;
+	const [characters, setCharacters] = useState<ICharactersProps>();
+
+	useEffect(() => {
+		getCharacters().then(setCharacters);
+	}, []);
+
+	return (
+		<div className='p-4 grid grid-cols-3 gap-6'>
+			{characters?.results.map(props => <CharacterCard key={v4()} {...props} />)}
+		</div>
+	);
 };
 
 export default HomePage;
